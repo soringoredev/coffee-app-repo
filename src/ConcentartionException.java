268fd3449b8fd529f65bc516d73a7b15815b026d
Sorin Gore <soringoredev@gmail.com>
2025-10-22 19:23:24 +0300

    feat: implement Concentration exc

--- src/ConcentartionException.java
+++ src/ConcentartionException.java
@@ -1,2 +1,17 @@
 public class ConcentartionException extends Exception {
+    // props
+    int c;
+
+    // constructor
+
+    public ConcentartionException(int c, String msg) {
+        super(msg);
+        this.c = c;
+    }
+
+    // getter
+
+    public int getC() {
+        return c;
+    }
 }
